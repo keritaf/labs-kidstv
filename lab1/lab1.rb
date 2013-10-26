03451bf60aa2e432a2c2ab3ffda54dced3171596
Valentine Zavadsky <saicheg@gmail.com>
2013-10-26 10:32:57 +0300

    lab1 is done

--- lab1/lab1.rb
+++ lab1/lab1.rb
@@ -13,18 +13,18 @@ F4 = Function.new('and', 3, F2, 6)
 F5 = Function.new('or',  F1, F4)
 F6 = Function.new('and', Not.new(F3), F5)
 
-FUNCTIONS = [F1, F2, F3, F4, F5, F6]
-
 node   = ask("Node(F1-F6): ") { |n| n.default = "F4" }
 value  = ask("Value(0|1): ")  { |val| val.default = "0" }
 
 tests = (0..2**INPUTS-1).map{|i| ("%0#{INPUTS}d" % i.to_s(2)).each_char.map(&:to_i)}
-r = tests.each_with_object([]) do |test, result|
+count = 0
+
+tests.each_with_object(0) do |test|
   $in = test
-  if Object.const_get(node).value == value.to_i
-    result << test
-    puts "#{test.inspect} => #{Object.const_get(node).value}"
+  if Object.const_get(node).value == value.to_i && F6.value == value.to_i
+    puts "#{test.inspect}"
+    count += 1
   end
 end
 
-puts "Total: #{r.count} tests"
+puts "Total: #{count} tests"
