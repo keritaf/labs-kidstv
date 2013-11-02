bbdb9650bdd4dfc8c9f425cd6ff8221086da3a28
Valentine Zavadsky <saicheg@gmail.com>
2013-11-02 09:29:53 +0300

    faults hunter

--- lab3/lab3.rb
+++ lab3/lab3.rb
@@ -4,14 +4,11 @@ require 'highline/import'
 
 tests = (0..2**Scheme::INPUTS-1).map{|i| ("%0#{Scheme::INPUTS}d" % i.to_s(2)).each_char.map(&:to_i)}
 
-scheme = Scheme.new(mock: {x0: 1})
-
 tests.each_with_object(0) do |test|
 
-  # scheme = Scheme.new
-  # puts "#{test.inspect} => #{scheme.process(test)}"
+  puts "#{test.inspect} => #{FaultHunter.new(test).faults.inspect}"
 
-  puts "#{test.inspect} => #{scheme.process(test)}"
+  # puts "#{test.inspect} => #{scheme.process(test)}"
 
 
   # if Object.const_get(node).value == value.to_i && F6.value == value.to_i
