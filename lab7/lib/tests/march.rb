60e3c92cc4b36d28e13fc7542aeb5ae0095a9465
Valentine Zavadsky <saicheg@gmail.com>
2013-11-09 11:03:47 +0300

    refactor march

--- lab7/lib/tests/march.rb
+++ lab7/lib/tests/march.rb
@@ -3,8 +3,6 @@ require_relative 'base'
 module Tests
   class March < Base
 
-    WINDOW = 12
-
     def perform
       raise NotImplementedError
     end
@@ -27,24 +25,20 @@ module Tests
       @r1 ||= ->(i) { @errors << i unless @memory[i] == 1 }
     end
 
-    def before(*args)
+    def down(*args)
       (@size - 1).downto(0) do |i|
         args.each { |arg| arg.call(i) }
       end
     end
 
-    def after(*args)
+    def up(*args)
       0.upto(@size - 1) do |i|
         args.each { |arg| arg.call(i) }
       end
     end
 
-    def random(*args)
-      rand(0.0..1.0) > 0.5 ? before(*args) : after(*args)
-    end
-
-    def num
-      (WINDOW-1 * rand(0.1..1.0)).round
+    def any(*args)
+      rand(0.0..1.0) > 0.5 ? up(*args) : down(*args)
     end
 
   end
--- lab7/lib/tests/march_a.rb
+++ lab7/lib/tests/march_a.rb
@@ -3,16 +3,13 @@ require_relative 'march'
 module Tests
   class MarchA < March
 
-    WINDOW = 15
-
     def perform
-      random(w0)
-      after(r0, w1, w0, w1)
-      after(r1, w0, w1)
-      before(r1, w0, w1, w0)
-      before(r0, w1, w0)
+      any(w0)
+      up(r0, w1, w0, w1)
+      up(r1, w0, w1)
+      down(r1, w0, w1, w0)
+      down(r0, w1, w0)
     end
 
   end
 end
-
